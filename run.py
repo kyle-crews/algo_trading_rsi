6306938e1264c16d0e85fad5792327f0df4b441b
Kyle Crews <dev.kylecrews@gmail.com>
2021-06-11 18:14:49 -0500

    added comments on code functionality

--- run.py
+++ run.py
@@ -8,6 +8,13 @@ from termcolor import colored as cl
 plt.style.use('fivethirtyeight')
 plt.rcParams['figure.figsize'] = (20, 10)
 
+# The first thing we did is to define a function named ‘get_historical_data’ that takes the stock’s symbol (‘symbol’) as a required parameter 
+# and the starting date of the historical data (‘start_date’) as an optional parameter. Inside the function, we are defining the API key 
+# and the URL and stored them into their respective variable. Next, we are extracting the historical data in JSON format using the ‘get’ function 
+# and stored it into the ‘raw_df’ variable. After doing some processes to clean and format the raw JSON data, we are returning it in the form of 
+# a clean Pandas dataframe. Finally, we are calling the created function to pull the historic data of IBM from the starting of 2020 and stored it 
+# into the ‘ibm’ variable.
+
 def get_historical_data(symbol, start_date = None):
     api_key = open(r'api_key.txt')
     api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}&outputsize=full'
